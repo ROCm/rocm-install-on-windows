9846451d27e9da720ac280a444d497b99700b42e
Alex Xu <alex.xu@amd.com>
2026-01-22 18:24:46 -0500

    add an extension

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,6 @@ exclude_patterns = ['temp']
 external_toc_path = "./sphinx/_toc.yml"
 
 external_projects_current_project = "rocm"
-html_theme = "rocm_docs_theme"
 html_theme_options = {
     "flavor": "generic",
     "header_title": f"ROCm™ Software 7.1.1",
@@ -49,6 +48,7 @@ html_theme_options = {
         "Support": "https://github.com/ROCm/ROCm/issues/new/choose",
     },
 }
+extensions = ["rocm_docs"]
 
 docs_core = ROCmDocs("HIP SDK installation (Windows)")
 docs_core.setup()
